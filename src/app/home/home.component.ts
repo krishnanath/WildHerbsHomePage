8c108891458b07be398fea23ae19adc14593300e
KRISHNANATH <krishnanath@KRISHNANATHs-MacBook-Air.local>
2019-05-08 13:05:18 +0530

    main-page with ringa mp4

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+  constructor() { }
+
+  ngOnInit() {
+    this.myStyle = {
+      // 'position': 'relative',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+  };
+  this.myParams = {
+    particles: {
+        number: {
+            value: 400,
+        },
+        color: {
+            value: '#ff0000'
+        },
+        shape: {
+            type: 'square',
+        },
+}
+};
+}
+  }
+
+
